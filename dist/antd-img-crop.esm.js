b4ab247f1c5008df833c305c831a846731236478
bilal1133 <bmehmood199@gmail.com>
2022-06-06 19:49:43 +0500

    .

--- dist/antd-img-crop.esm.js
+++ dist/antd-img-crop.esm.js
@@ -108,10 +108,10 @@ var EasyCrop = forwardRef(function (props, ref) {
     var _c = useState(INIT_ZOOM), zoomVal = _c[0], setZoomVal = _c[1];
     var _d = useState(INIT_ROTATE), rotateVal = _d[0], setRotateVal = _d[1];
     var cropPixelsRef = useRef({ width: 0, height: 0, x: 0, y: 0 });
-    var _e = useState({ height: 0, width: 0 }), mediaSize = _e[0], setmediaSize = _e[1];
+    var _e = useState({ height: 0, width: 0 }), mediaSize = _e[0], setMediaSize = _e[1];
     var onMediaLoaded = useCallback(function (mediaSize) {
         var width = mediaSize.width, height = mediaSize.height;
-        setmediaSize({ width: width, height: height });
+        setMediaSize({ width: width, height: height });
         var ratioWidth = height * aspect;
         if (width > ratioWidth) {
             setCropSize({ width: ratioWidth, height: height });
