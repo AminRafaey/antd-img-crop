dff8f105e64b2ed86213de1539bb0097cbe98434
bilal1133 <bmehmood199@gmail.com>
2022-06-07 14:25:40 +0500

    Added height and width in logs

--- src/img-crop.tsx
+++ src/img-crop.tsx
@@ -202,7 +202,7 @@ const ImgCrop = forwardRef<Cropper, ImgCropProps>((props, ref) => {
       ctx.fillRect(0, 0, cropWidth, cropHeight);
 
       ctx.drawImage(imgSource, cropX, cropY, cropWidth, cropHeight, 0, 0, cropWidth, cropHeight);
-      console.log('-=-=-=> CTx', JSON.stringify(ctx));
+      console.log('-=-=-=> CTx', ctx?.canvas?.style?.width, ctx?.canvas?.style?.height);
     }
 
     // get the new image
